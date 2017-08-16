54accff6e1c80f5cb5b1d2dc40a112618b2d8e61
Jakhar <vikram.jakhar@tothenew.com>
2017-08-16 13:11:04 +0530

    parsing changes

--- util/util.go
+++ util/util.go
@@ -28,19 +28,20 @@ type Reported struct {
 }
 
 type Payload struct {
-	NodeID    string `json:"NodeID"`
-	TimeStamp string `json:"TimeStamp"`
-	CO        float64 `json:"CO"`
-	Temp      float64 `json:"temp"`
-	Hum       float64 `json:"hum"`
-	Pre       float64 `json:"pre"`
-	O3        float64 `json:"O3"`
-	SO2       float64 `json:"SO2"`
-	NO2       float64 `json:"NO2"`
-	PM1       float64 `json:"PM1"`
-	PM2_5     float64 `json:"PM2.5"`
-	PM10      float64 `json:"PM10"`
-	BatLevel  float64 `json:"BatLevel"`
+	Measurement string `json:"Measurement"`
+	NodeID      string `json:"NodeID"`
+	TimeStamp   string `json:"TimeStamp"`
+	CO          float64 `json:"CO"`
+	Temp        float64 `json:"temp"`
+	Hum         float64 `json:"hum"`
+	Pre         float64 `json:"pre"`
+	O3          float64 `json:"O3"`
+	SO2         float64 `json:"SO2"`
+	NO2         float64 `json:"NO2"`
+	PM1         float64 `json:"PM1"`
+	PM2_5       float64 `json:"PM2.5"`
+	PM10        float64 `json:"PM10"`
+	BatLevel    float64 `json:"BatLevel"`
 }
 
 func ParsePayload(data string) JsonPayload {
@@ -61,19 +62,20 @@ func payload(dataMap map[string]interface{}) JsonPayload {
 		State: State{
 			Reported: Reported{
 				Data: Payload{
-					NodeID:    strings.TrimSpace(dataMap["NodeID"].(string)),
-					TimeStamp: strings.TrimSpace(dataMap["TimeStamp"].(string)),
-					CO:        ToFloat64(dataMap["CO"]),
-					Temp:      ToFloat64(dataMap["temp"]),
-					Hum:       ToFloat64(dataMap["hum"]),
-					Pre:       ToFloat64(dataMap["pre"]),
-					O3:        ToFloat64(dataMap["O3"]),
-					SO2:       ToFloat64(dataMap["SO2"]),
-					NO2:       ToFloat64(dataMap["NO2"]),
-					PM1:       ToFloat64(dataMap["PM1"]),
-					PM2_5:     ToFloat64(dataMap["PM2.5"]),
-					PM10:      ToFloat64(dataMap["PM10"]),
-					BatLevel:  ToFloat64(dataMap["BatLevel"]),
+					Measurement: "sensor",
+					NodeID:      strings.TrimSpace(dataMap["NodeID"].(string)),
+					TimeStamp:   strings.TrimSpace(dataMap["TimeStamp"].(string)),
+					CO:          ToFloat64(dataMap["CO"]),
+					Temp:        ToFloat64(dataMap["temp"]),
+					Hum:         ToFloat64(dataMap["hum"]),
+					Pre:         ToFloat64(dataMap["pre"]),
+					O3:          ToFloat64(dataMap["O3"]),
+					SO2:         ToFloat64(dataMap["SO2"]),
+					NO2:         ToFloat64(dataMap["NO2"]),
+					PM1:         ToFloat64(dataMap["PM1"]),
+					PM2_5:       ToFloat64(dataMap["PM2.5"]),
+					PM10:        ToFloat64(dataMap["PM10"]),
+					BatLevel:    ToFloat64(dataMap["BatLevel"]),
 				},
 			},
 		},
@@ -91,11 +93,11 @@ type OSArgs struct {
 
 func ParseArgs() OSArgs {
 	port := flag.String("port", "9292", "TCP port to listen")
-	certFile := flag.String("certFile", "/opt/certificates/certificate.pem.crt", "Certificate file")
-	privateKeyFile := flag.String("privateKeyFile", "/opt/certificates/private.pem.key", "Private key file")
+	certFile := flag.String("certFile", "/opt/certificates/IoI-certificate.pem.crt", "Certificate file")
+	privateKeyFile := flag.String("privateKeyFile", "/opt/certificates/IoI-private.pem.key", "Private key file")
 	host := flag.String("host", "a2kbv0xdj5homp.iot.us-east-1.amazonaws.com", "AWS IoT host name")
 	shadowUpdate := flag.String("shadowUpdate", "$aws/things/IoI/shadow/update", "Shadow update topic name")
-	clientID := flag.String("clientID", "Client", "Shadow update topic name")
+	clientID := flag.String("clientID", "tcp-server-client", "client name")
 	flag.Parse()
 	return OSArgs{
 		Port:           *port,
